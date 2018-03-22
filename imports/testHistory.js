9f428119c16e9768cdfca1baf2c830a0c210dd1c
steveross1975 <steveross1975@gmail.com>
2018-03-22 10:07:03 +0100

    added testHistory.js for the creation of a MemoryHistory object for test purposes and updated YourComponent.test.js

---
+++ imports/testHistory.js
@@ -0,0 +1,5 @@
+// For use ONLY WITH TEST FILES THAT REQUIRE A HISTORY OBJ.
+import createMemoryHistory from 'history/createMemoryHistory';
+
+const testHistory = createMemoryHistory();
+export default testHistory;
--- imports/ui/YourComponent.test.js
+++ imports/ui/YourComponent.test.js
@@ -10,17 +10,64 @@ Enzyme.configure({ adapter: new Adapter() });
 
 if (Meteor.isClient) {
   describe('YourComponent', function () {
-    it('should set button text to logout', function () {
+    //Variable Definition
+    let call;
+    let memoryHistory;
+    let Session;
+    //Lifecycle hook (is executed before each it)
+    beforeEach(() => {
+      call = expect.createSpy();
+      memoryHistory = {
+        ...testHistory,
+        push: expect.createSpy()
+      };
+      Session = {
+        set: expect.createSpy()
+      };
+    });
+    //Lifecycle hook (is executed after each it)
+    afterEach(() => {
+      //your code here
+    });
+    //standard test cases
+    it('should do something', function () {
       const wrapper = mount( <YourComponent title="Test title"/> );
       const buttonText = wrapper.find('button').text();
 
       expect(buttonText).toBe('Logout');
     });
-    it('should set title to the one provided as h1 text', function () {
+    it('should do something', function () {
       const title = 'Pick your title';
       const wrapper = mount( <YourComponent title={title}/> );
       const titleFromPH = wrapper.find('h1').text();
       expect(titleFromPH).toBe(title);
-    })
+    });
+    //test cases with call to a function and that use a history object
+    it('should do something', function () {
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/']} initialIndex={0}>
+          <Editor history={memoryHistory} call={call} selectedNoteId={notes[0]._id} note={notes[0]} Session={Session}/>
+        </MemoryRouter>
+      );
+      wrapper.find('button').simulate('click');
+      expect(call).toHaveBeenCalledWith('notes.remove', notes[0]._id);
+      expect(memoryHistory.push).toHaveBeenCalledWith('/dashboard');
+    });
+    it('should do something', function () {
+      const newBody = 'This is my new body text'
+      const wrapper = mount(
+        <MemoryRouter initialEntries={['/']} initialIndex={0}>
+          <Editor history={memoryHistory} call={call} selectedNoteId={notes[0]._id} note={notes[0]} Session={Session}/>
+        </MemoryRouter>
+      );
+      wrapper.find('textarea').simulate('change', {
+        target: {
+          value: newBody
+        }
+      });
+      expect(wrapper.find(Editor).instance().state.body).toBe(newBody);
+      expect(call).toHaveBeenCalledWith('notes.update', notes[0]._id, {body: newBody});
+    });
+
   });
 }
